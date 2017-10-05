f089a809399510e3e6d9e09d06dab4e959f5af6e
slwu89 <slwu89@berkeley.edu>
2017-10-05 15:31:40 -0700

    Initial Commit

---
+++ InfectionSim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InfectionSim
+//
+//  Created by Sean Wu on 10/5/17.
+//  Copyright © 2017 Sean Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
