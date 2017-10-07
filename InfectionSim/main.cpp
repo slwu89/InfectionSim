7c7cd0923afa7663c101d4a2d4b103804245e78a
slwu89 <slwu89@berkeley.edu>
2017-10-07 16:08:00 -0700

    bridge pattern

--- InfectionSim/main.cpp
+++ InfectionSim/main.cpp
@@ -4,7 +4,7 @@
 #include "human.hpp"
 #include "house.hpp"
 #include "block.hpp"
-
+#include "pathogen.hpp"
 
 int main() {
     
@@ -12,27 +12,34 @@ int main() {
     int nHouse = 2;
     int nHuman = 5;
     
-    std::vector<block> city;
+    std::vector<block*> city;
     city.reserve(nBlock);
     for(int i=0; i<nBlock; i++){
         std::cout << "block i: " << i << std::endl;
-        city.push_back(block(i));
-        
+        block* b = new block(i);
+        city.push_back(b);        
         for(int j=0; j<nHouse; j++){
             std::cout << "house j: " << j << std::endl;
             house* h = new house(j);
-            city[i].add_house(h);
-            
+            city[i]->add_house(h);
             for(int k=0; k<nHuman; k++){
                 std::cout << "human k: " << k << std::endl;
                 human* p = new human(k);
-                city[i].get_house(j)->add_human(p);
+                city[i]->get_house(j)->add_human(p);
             }
         }
     }
     
+    // pathogens
+    pathogen_abstract* usePathogen = 0;
+    pathogen_implementation* p_vivax_implementation = new pathogen_p_vivax;
+    pathogen_implementation* p_falciparum_implementation = new pathogen_p_falciparum;
     
+    usePathogen = new pathogen(p_falciparum_implementation);
+    usePathogen->fun();
     
+    usePathogen = new pathogen(p_vivax_implementation);
+    usePathogen->fun();
 
     return 0;
 }
--- InfectionSim/pathogen.hpp
+++ InfectionSim/pathogen.hpp
@@ -1,40 +1,61 @@
-//
-//  pathogen.hpp
-//  InfectionSim
-//
-//  Created by Sean Wu on 10/6/17.
-//  Copyright © 2017 Sean Wu. All rights reserved.
-//
-
 #ifndef pathogen_hpp
 #define pathogen_hpp
 
 class human; // forward declare human
 
 #include <stdio.h>
+#include <iostream>
 
-class pathogen_interface {
+// implemented interface
+class pathogen_abstract {
 public:
-    virtual ~pathogen_interface();
     virtual void fun() = 0;
 };
 
+// internal implementation
 class pathogen_implementation {
 public:
-    virtual ~pathogen_implementation();
-    virtual void fun1() = 0;
+    virtual void fun_impl() = 0;
 };
 
-class pathogen_bridge : public pathogen_interface {
+// bridge
+class pathogen_bridge : public pathogen_abstract {
 protected:
     pathogen_implementation* p_implementation;
-    
+
 public:
     pathogen_bridge(pathogen_implementation* p_backend){
         p_implementation = p_backend;
     }
 };
 
+// pathogen class
+class pathogen : public pathogen_bridge {
+public:
+    pathogen(pathogen_implementation* p_backend) : pathogen_bridge(p_backend) {}
+    
+    void fun(){
+        std::cout << "pathogen strain ";
+        p_implementation->fun_impl();
+    }
+};
+
+// p. falciparum
+class pathogen_p_falciparum : public pathogen_implementation {
+public:
+    void fun_impl(){
+        std::cout << "plasmodium falciparum" << std::endl;
+    }
+};
+
+// p. vivax
+class pathogen_p_vivax : public pathogen_implementation {
+public:
+    void fun_impl(){
+        std::cout << "plasmodium vivax" << std::endl;
+    }
+    
+};
 
 
 #endif /* pathogen_hpp */
